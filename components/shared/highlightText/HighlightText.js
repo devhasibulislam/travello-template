d70a685f38a2d9a0eae19b402a75458ac68ceeed
devhasibulislam <devhasibulislam@gmail.com>
2023-11-24 20:37:11 +0600

    highlight dir init

---
+++ components/shared/highlightText/HighlightText.js
@@ -0,0 +1,22 @@
+/**
+ * Title: Write a program using JavaScript on HighlightText
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/in/devhasibulislam
+ * Facebook: https://facebook.com/in/devhasibulislam
+ * Instagram: https://instagram.com/in/devhasibulislam
+ * Twitter: https://twitter.com/in/devhasibulislam
+ * Pinterest: https://pinterest.com/in/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 16, August 2023
+ */
+
+import React from "react";
+
+const HighlightText = ({ children }) => {
+  return <span className="text-primary">{children}</span>;
+};
+
+export default HighlightText;
