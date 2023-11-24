df45fe57ad375093cca3a40a7a9903858008a021
devhasibulislam <devhasibulislam@gmail.com>
2023-11-24 20:34:24 +0600

    app dir init

---
+++ app/store.js
@@ -0,0 +1,39 @@
+/**
+ * Title: Write a program using JavaScript on Store
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/in/devhasibulislam
+ * Facebook: https://facebook.com/in/devhasibulislam
+ * Instagram: https://instagram.com/in/devhasibulislam
+ * Twitter: https://twitter.com/in/devhasibulislam
+ * Pinterest: https://pinterest.com/in/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 17, August 2023
+ */
+
+import authSlice from "@/features/auth/authSlice";
+import rentSlice from "@/features/rent/rentSlice";
+import rentFilterSlice from "@/features/rentFilter/rentFilterSlice";
+import travelAvailabilitySlice from "@/features/travelAvailability/travelAvailabilitySlice";
+import userSlice from "@/features/user/userSlice";
+import { travelloApi } from "@/services/travello";
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
+
+export const store = configureStore({
+  reducer: {
+    [travelloApi.reducerPath]: travelloApi.reducer,
+    travelAvailability: travelAvailabilitySlice,
+    rentFilter: rentFilterSlice,
+    user: userSlice,
+    auth: authSlice,
+    rent: rentSlice,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(travelloApi.middleware),
+  devTools: process.env.NODE_ENV !== "production",
+});
+
+setupListeners(store.dispatch);
