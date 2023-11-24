87320edf777bea470a6e925aa24e0069fda168b1
devhasibulislam <devhasibulislam@gmail.com>
2023-11-24 20:39:41 +0600

    auth dir init

---
+++ features/auth/authSlice.js
@@ -0,0 +1,32 @@
+/**
+ * Title: Write a program using JavaScript on AuthSlice
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 17, November 2023
+ */
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    addUser: (state, action) => {
+      state = action.payload;
+      return state;
+    },
+  },
+});
+
+export const { addUser } = authSlice.actions;
+export default authSlice.reducer;
