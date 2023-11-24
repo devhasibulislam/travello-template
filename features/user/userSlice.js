a9cb1acb6e2eb4a0e1da2ce0752f63827839353a
devhasibulislam <devhasibulislam@gmail.com>
2023-11-24 20:40:08 +0600

    user dir init

---
+++ features/user/userSlice.js
@@ -0,0 +1,32 @@
+/**
+ * Title: Write a program using JavaScript on UserSlice
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 17, November 2023
+ */
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state = action.payload;
+      return state;
+    },
+  },
+});
+
+export const { setUser } = userSlice.actions;
+export default userSlice.reducer;
