8fdda2fb7eb3ee92b1f7e29312e7044583b9c701
devhasibulislam <devhasibulislam@gmail.com>
2024-02-07 11:13:28 +0600

    admin can add to cart others product

--- pages/api/cart/index.js
+++ pages/api/cart/index.js
@@ -36,7 +36,7 @@ export default async function handler(req, res) {
             });
           }
 
-          authorization("user")(req, res, async (err) => {
+          authorization("user", "admin")(req, res, async (err) => {
             if (err) {
               return res.send({
                 success: false,
